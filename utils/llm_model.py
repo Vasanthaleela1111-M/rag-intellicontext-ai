f798821d502e93bfdfd9d4857d25e5402f8a1587
Vasanthaleela  M <vasanthaleela0207@gmail.com>
2026-08-23 23:40:01 +0530

    Update llm_model.py

--- utils/llm_model.py
+++ utils/llm_model.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_groq import ChatGroq
 
 llm = ChatGroq(
-    model="llama-3.1-8b-instant",
+    model="openai/gpt-oss-20b",
     groq_api_key=st.secrets["GROQ_API_KEY"],
     temperature=0
 )
