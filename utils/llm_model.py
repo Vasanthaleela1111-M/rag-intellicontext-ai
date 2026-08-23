9f1f3233943b8fc8a05ffb1a4a810e0668d28270
Vasanthaleela-M <vasanthaleela0207@gmail.com>
2026-08-24 01:05:30 +0530

    feat: switch to Groq GPT-OSS model

--- utils/llm_model.py
+++ utils/llm_model.py
@@ -3,7 +3,7 @@ from langchain_groq import ChatGroq
 
 def get_llm():
     return ChatGroq(
-        model="mixtral-8x7b-32768",
+        model="openai/gpt-oss-20b",
         groq_api_key=st.secrets["GROQ_API_KEY"],
         temperature=0
     )
